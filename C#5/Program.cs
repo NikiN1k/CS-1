2ccf60729e2707b840bd11b57b0fcb7c81f555ab
Nik <ZuzinNikita@mail.ru>
2022-12-10 12:39:20 +0300

    ***я

---
+++ C#5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя,");
+string username = Console.ReadLine();
+if(username == "Masha")
+{
+    Console.Writeline("Hi, Masha");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.Writeline(username);
+}

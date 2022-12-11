88b75bcd01d38925ec1c87d5b4b0727313614085
Nik <ZuzinNikita@mail.ru>
2022-12-11 10:05:35 +0300

    Цикл

---
+++ C#7/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+
+Console.SetCursorPosition (xa,ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xb,yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xc,yc);
+Console.WriteLine("+");
+
+int x = xa,ya;
+
+int count = 10;
+
+while(count<10);
+{int what = new Random().Next(0,3);
+if (what == 0)
+{x = (x+xa) / 2;
+ y = (y+ya) / 2;    
+}
+
+
+
+
+
+}
